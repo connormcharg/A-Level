02fe7655a083abbbe17cf5d10caf8bb55f67e50c
Connor McHarg <68478521+MaximumFire@users.noreply.github.com>
2024-01-15 18:31:16 +0000

    queue designing

--- Projects/AbstractDataTypes/classes/Queue.cs
+++ Projects/AbstractDataTypes/classes/Queue.cs
@@ -1,46 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AbstractDataTypes.classes
 {
     /// <summary>
-    /// My implementation of a linear queue.
+    /// My implementation of a dynamic linear queue.
     /// </summary>
     public class Queue
     {
-        private int[] data;
-        private int front;
-        private int rear;
-
-        private int[] GetArr(int n)
-        {
-            int[] arr = new int[n];
-            for (int i = 0; i < n; i++)
-            {
-                arr[i] = i;
-            }
-            return arr;
-        }
+        public QueueNode Front { get; set; }
+        public QueueNode Back { get; set; }
 
         public Queue()
         {
-            data = new int[10];
-            front = 0;
-            rear = 0;
+            Front = null; Back = null;
         }
 
-        public Queue(int n)
+        public int Length()
         {
-            data = new int[n];
-            front = 0;
-            rear = 0;
+            if (Front == null) return 0;
+
+            int n = 1;
+            Node current = Front;
+
+            while (current.Next != null)
+            {
+                n++;
+                current = current.Next;
+            }
+
+            return n;
         }
 
-        public void Enqueue()
+        public void Enqueue(QueueNode n)
         {
+            
 
         }
     }
---
+++ Projects/AbstractDataTypes/classes/QueueNode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractDataTypes.classes
+{
+    public class QueueNode : Node
+    {
+        public Node Prev { get; set; }
+
+        public QueueNode(int d) : base(d)
+        {
+            Prev = null;
+        }
+
+        public QueueNode() : base()
+        {
+            Prev = null;
+        }
+    }
+}
