4662c64921793520d16080f7c7cdfe1bddd8deba
Connor McHarg <68478521+MaximumFire@users.noreply.github.com>
2024-01-26 19:22:32 +0000

    nea ideas and rendering

---
+++ Projects/RenderingV2/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace RenderingV2
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrTick = new System.Windows.Forms.Timer(this.components);
+            this.lblFPS = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tmrTick
+            // 
+            this.tmrTick.Enabled = true;
+            this.tmrTick.Interval = 1;
+            this.tmrTick.Tick += new System.EventHandler(this.tmrTick_Tick);
+            // 
+            // lblFPS
+            // 
+            this.lblFPS.AutoSize = true;
+            this.lblFPS.Location = new System.Drawing.Point(13, 425);
+            this.lblFPS.Name = "lblFPS";
+            this.lblFPS.Size = new System.Drawing.Size(30, 13);
+            this.lblFPS.TabIndex = 0;
+            this.lblFPS.Text = "0 fps";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblFPS);
+            this.Name = "frmMain";
+            this.Text = "Main";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer tmrTick;
+        private System.Windows.Forms.Label lblFPS;
+    }
+}
+
---
+++ Projects/RenderingV2/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RenderingV2
+{
+    public partial class frmMain : Form
+    {
+        Bitmap image {  get; set; }
+        Graphics g { get; set; }
+        int x { get; set; }
+        long _frameCount { get; set; }
+        DateTime _lastCheckTime = DateTime.Now;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            image = new Bitmap(this.Width, this.Height);
+            this.BackgroundImage = image;
+            g = Graphics.FromImage(image);
+        }
+
+        private void tmrTick_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < this.Height; i++)
+            {
+                for (int j = 0; j < this.Width; j++)
+                {
+                    image.SetPixel(j, i, Color.Aqua);
+                }
+            }
+            g.DrawEllipse(new Pen(Color.Blue), new Rectangle(x, 50, 200, 100));
+            this.BackgroundImage = image;
+            this.Refresh();
+            x += 10;
+            _frameCount += 1;
+            lblFPS.Text = $"{DisplayFps()} fps";
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            g.Dispose();
+            image.Dispose();
+        }
+
+        private double DisplayFps()
+        {
+            double secondsElapsed = (DateTime.Now - _lastCheckTime).TotalSeconds;
+            double fps = _frameCount / secondsElapsed;
+            _lastCheckTime = DateTime.Now;
+            _frameCount = 0;
+            return fps;
+        }
+    }
+}
