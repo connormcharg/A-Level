dbc01b317bee560b77e308d58c32ded7e767b096
Connor McHarg <68478521+MaximumFire@users.noreply.github.com>
2024-02-28 22:04:57 +0000

    opengl testing

---
+++ Projects/OpenGLTesting/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenGLTesting.classes;
+
+namespace OpenGLTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Game game = new Game(800, 600, "LearnOpenTK"))
+            {
+                game.Run((double)60);
+            }
+        }
+    }
+}
---
+++ Projects/OpenGLTesting/classes/Game.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace OpenGLTesting.classes
+{
+    public class Game : GameWindow
+    {
+        float[] vertices = {
+            -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+             0.5f, -0.5f, 0.0f, //Bottom-right vertex
+             0.0f,  0.5f, 0.0f  //Top vertex
+        };
+        int VertexBufferObject;
+        int VertexArrayObject;
+        Shader shader;
+
+        public Game(int width, int height, string title) : base(width, height, GraphicsMode.Default, title) { }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            KeyboardState input = Keyboard.GetState();
+
+            if (input.IsKeyDown(Key.Escape))
+            {
+                Exit();
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            VertexBufferObject = GL.GenBuffer();
+            VertexArrayObject = GL.GenVertexArray();
+
+            shader = new Shader("shaders/shader.vert", "shaders/shader.frag");
+        }
+
+        private void DrawTriangle()
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            GL.BindVertexArray(VertexArrayObject);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            shader.Use();
+
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+        }
+
+        protected override void OnUnload(EventArgs e)
+        {
+            base.OnUnload(e);
+
+            shader.Dispose();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            DrawTriangle();
+
+            SwapBuffers();
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, Width, Height);
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.DeleteBuffer(VertexBufferObject);
+        }
+    }
+}
