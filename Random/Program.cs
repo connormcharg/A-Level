8c0e8c20512f779f2895ed7b38e3e6c330dca988
Connor McHarg <68478521+connormcharg@users.noreply.github.com>
2025-07-15 20:51:45 +0100

    Update Program.cs

--- Random/Program.cs
+++ Random/Program.cs
@@ -2,18 +2,8 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine(Multiply(2, 6));
-        }
-
-        static int Multiply(int a, int b)
-        {
-            if (b == 1)
-            {
-                return a;
-            }
-            return a + Multiply(a, b - 1);
         }
     }
 }
